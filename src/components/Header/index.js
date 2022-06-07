dd8c31d35af97eadcbaefc205ec8d0d59dd67ad2
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-07 13:57:52 +0300

    add Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+
+function Header() {
+  return (
+    <div className='header-div'>
+        <img src = "https://neal.fun/spend/billgates.jpg" className='header-img'/>
+        Spend Bill Gates' Money
+    </div>
+  )
+}
+
+export default Header;
