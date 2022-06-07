851ec7f0108c8d7ce12e8825653acf866fa7e61a
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-07 14:08:06 +0300

    add Money

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Money from './components/Money';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Money />
     </div>
   );
 }
---
+++ src/components/Money/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./style.css"
+
+function Money() {
+  return (
+    <div className='money-div'>$100,000,000,000</div>
+  )
+}
+
+export default Money;
