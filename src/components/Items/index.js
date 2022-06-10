67a7622aaa71b1900477c1a70d0df9b20d050896
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-10 14:35:03 +0300

    add fetchProducts , extraReducers, products map, useEffect

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import {useEffect} from 'react';
+import { useDispatch , useSelector} from 'react-redux';
+import { fetchProducts } from '../../redux/products/productsSlice';
 import "./style.css"
 
 function Items() {
+  const dispatch = useDispatch();
+  const products= useSelector(state => state.products.items);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  })
+
   return (
-    <div className='items-div'>Items</div>
+    <div className='items-div'>
+      {
+        products.map((product) => (
+          <div key={product.id}>
+            <img src={product.image}/>
+            <span>{product.productPrice}</span>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
--- src/redux/products/productsSlice.js
+++ src/redux/products/productsSlice.js
@@ -1,11 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchProducts = createAsyncThunk(
+  "products/getProducts",
+  async () => {
+    const res = await axios(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/spendMoneyJSON`
+    ); 
+    console.log(res.data);
+    return res.data;
+  }
+);
 
 export const productsSlice = createSlice({
-    name: "products",
-    initialState: {
-        items: "",
-    }
-})
+  name: "products",
+  initialState: {
+    items: [],
+  },
+  reducers: {},
 
+  extraReducers: {
+      [fetchProducts.fulfilled]: (state, action) => {
+          state.items = action.payload;
+      }
+  }
+});
 
 export default productsSlice.reducer;
