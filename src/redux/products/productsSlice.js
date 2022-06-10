b77263d1286dcbc8e63d8834be8902383fa99509
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-10 18:36:48 +0300

    add updateCount

--- src/redux/products/productsSlice.js
+++ src/redux/products/productsSlice.js
@@ -6,7 +6,7 @@ export const fetchProducts = createAsyncThunk(
   async () => {
     const res = await axios(
       `${process.env.REACT_APP_API_BASE_ENDPOINT}/spendMoneyJSON`
-    ); 
+    );
     console.log(res.data);
     return res.data;
   }
@@ -16,14 +16,31 @@ export const productsSlice = createSlice({
   name: "products",
   initialState: {
     items: [],
+    value: 100000000000,
+    money: 100000000000,
   },
-  reducers: {},
+  reducers: {
+    updateCount: (state, action) => {
+      const { id, count } = action.payload;
+      const x = fetchProducts.find((item) => item.id === id);
+      x.count = count;
+      let price = 0;
+
+      fetchProducts.map((item) => {
+        //  Fiyat = miktar * ücret
+        price += Number(item.count) * Number(item.productPrice);
+      });
+      //para = ilk değer - fiyat
+      state.value = Number(state.money) - Number(price);
+    },
+  },
 
   extraReducers: {
-      [fetchProducts.fulfilled]: (state, action) => {
-          state.items = action.payload;
-      }
-  }
+    [fetchProducts.fulfilled]: (state, action) => {
+      state.items = action.payload;
+    },
+  },
 });
 
+export const {updateCount} = productsSlice.actions;
 export default productsSlice.reducer;
