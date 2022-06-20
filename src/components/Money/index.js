290a24fa457a4f9ad6c2c0c97d764a45ef031e2d
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-20 17:04:10 +0300

    add money comp. changes

--- src/components/Money/index.js
+++ src/components/Money/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import "./style.css"
+import CountUp from 'react-countup';
 
 function Money() {
+  const value = useSelector(state => state.products.value);
+  const money = useSelector(state => state.products.money);
   return (
-    <div className='money-div'>$100,000,000,000</div>
+    
+    <div className='money-div'>
+    <CountUp 
+    start={money}
+    end={value}
+    duration={1} 
+    prefix="$ " 
+    separator=","/>
+    </div>
   )
 }
 
