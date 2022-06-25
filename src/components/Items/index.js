fbf57166f7489660c8b4010e4aa507c2c03a18dd
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-25 17:03:43 +0300

    add handleChange reducer

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,9 +1,10 @@
-import {useEffect, useState, useCallback} from 'react';
+import {useEffect} from 'react';
 import { useDispatch , useSelector} from 'react-redux';
-import { fetchProducts, updateCount, handleChange } from '../../redux/products/productsSlice';
+import { fetchProducts, handleChange } from '../../redux/products/productsSlice';
 import "./style.css";
 
-function Items({id}){
+function Items(){
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -11,77 +12,7 @@ function Items({id}){
   },[dispatch]);
 
   const items = useSelector((state) => state.products.items);
-  const item = items !== [] || items !== undefined ? items.find((x) => x.id === id) : null;
   
-  console.log('items', items)
-  console.log('item ', item);
-
-  const money = useSelector(state => state.products.money);
-
-  const [count, setCount] = useState(items.count);
-  const [buyable , setBuyable] = useState(false); 
-  const [sellable, setSellable ] = useState(true);
-
-  let maxBuy = Math.floor(money / items.productPrice);
-  let max = Number(count) + Number(maxBuy); 
-  // değeri ürün fiyatına bölüyoruz, ürün adedi ile topluyoruz ne kadar alınabilir onu hesaplıyoruz.
-
-  //sell butonu sıfır ve sıfırdan küçükse disable olması için.
-  //for callback error (returns a memoized callback function, caching a value so that it does not need to be recalculated)
-  const control = useCallback(()=>{
-    if(count > 0){
-      setSellable(false);
-    }
-    if(count == 0){
-      setSellable(true);
-    }
-  },[count]);
-
-  useEffect(() => {
-    control();
-  },[control]);
-  
-  useEffect(() => {
-    if(items.productPrice > money){ //ürün fiyatı paramızdan fazlaysa true yap.
-      setBuyable(true);
-    }
-    if(items.productPrice <= money){
-      setBuyable(false);
-    }
-  },[money, items.productPrice]);
-
-  //count(kaç adet) değerlerimiz için.
-  const handleChange = async (value) =>{
-    if(value>max && money>0 ){
-      setCount(max)
-    }
-    else if(value<0){
-      setCount(0);
-    }
-    else if(money == 0 && value<count){
-      setCount(value);
-    }
-    else{
-      setCount(value);
-    }
-    await dispatch(updateCount({id: items.id, count: count}))
-  }
-
-
-  const buy = () => {
-    handleChange(Number(count) + 1);
-     }
- 
-   const sell = () => {
-     handleChange(Number(count) - 1);
-   }
-  
-  //  const handleClickBtn = (id, count) => {
-  //   dispatch(handleChange({id: items.id, count: count}))
-  //   dispatch(updateCount({id: items.id, count: count}));
-  // }
-  
-
   return (
     <div className='items-div'>
       {
@@ -93,19 +24,22 @@ function Items({id}){
             <span style={{color: "green"}}>${product.productPrice}</span> 
             <br />
             <div>
-              {/* <button className='btn' disabled={sellable} onClick={() => handleClickBtn(product.id, product.count)}>Sell</button>           */}
-              {/* <input type='number' className='count-span' value={product.count} onChange={(e)=>dispatch(updateCount(e.target.value))} /> */}
-              
-              <button className='btn' disabled={sellable} onClick={() => sell()} style={{backgroundColor: "green", color: "white"}}>Buy</button>
-               <input type='number' className='count-span' value={product.count} onChange={(e)=>handleChange(e.target.value)} />
-              <button className='btn' disabled={buyable} onClick={() => buy()} style={{backgroundColor: "green", color: "white"}}>Buy</button>
+              <button className='btn' 
+              disabled={product.count === "0"} 
+              onClick={(e) => dispatch(handleChange({id: product.id, count: Number(product.count), newCount: Number(product.count - 1)}))}>
+              Sell</button>
+               <input type='number' min="0" className='count-span' value={product.count} 
+               onChange={(e)=>dispatch(handleChange({id: product.id, count: Number(product.count), newCount: Number(e.target.value)}))} />
+              <button className='btn' 
+              // disabled={buyable} 
+              onClick={(e) => dispatch(handleChange({id: product.id, count: Number(product.count), newCount: Number(product.count + 1)}))}
+              style={{backgroundColor: "green", color: "white"}}>Buy</button>
               </div>        
           </div>
         ))
       }
     </div>
   );
-
 }
 
 
--- src/redux/products/productsSlice.js
+++ src/redux/products/productsSlice.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
@@ -16,70 +16,46 @@ export const productsSlice = createSlice({
   name: "products",
   initialState: {
     items: [],
-    money: 100000000000,
-    initialMoney:100000000000,
+    oldMoney: 100000000000, //paramız (100 milyon).
+    newMoney: 100000000000, //değişen paramızın değeri.
   },
-  reducers: { 
-    updateCount: (state, action) => {
-     
-      if(state.items !== [] || state.items !== undefined){
-        const {id,count} = action.payload;
-        const item = state.items.find((x) => x.id === id);
-        console.log('item', item);
-        item.count = count;
-      }
-      // console.log('items :>> ', state.items);
-      // // const item = state.items.find((x) => x.id === action.payload.id);
-      // console.log('item', item);
-      // item.count = action.payload.count;
-      // const item = state.items.find((x) => x.id === id);
-            // item.count=count;
-            let price=0;
+  reducers: {
+    handleChange: (state, action) => {
+      const { id, count, newCount } = action.payload;
+      console.log("action", action);
+      console.log("action.payload ", action.payload);
+      console.log("id, count, newCount", id, count, newCount);
 
-          state.items.map((tmp)=>{
-              //  Fiyat = miktar * ücret
-              return price += Number(tmp.count) * Number(tmp.productPrice);
-            });
-             // para = ilk değer - fiyat
-          return state.money= Number(state.initialMoney)-Number(price);
+      const item = state.items.find((item) => item.id === id);
+      console.log("item", item);
+
+      const countDifference = newCount - count; //count adedimiz , newCount basınca değişecek değerimiz.
+
+      const totalPrice = item.productPrice * Math.abs(countDifference);
+
+      if (countDifference > 0) { //alım
+              if (totalPrice > state.newMoney) {
+                alert("You don't have enough money!");
+                return; //işlem yapma
+              } else {
+                state.newMoney = state.oldMoney - totalPrice;
+                item.count = newCount;
+                state.oldMoney = state.newMoney; //bunu yazmazsak diğer item aldığımızda tekrar 100 milyondan fiyat düşüyor.
+              }             
+      } else { //satış
+        state.newMoney = state.oldMoney + totalPrice;
+        item.count = newCount;
+        state.oldMoney = state.newMoney; //bunu yazmazsak diğer item satışında 100 milyona ekleme yapıyor. 
+        //Tekrar tekrar parayı 100 milyon yapma paramız eksilen/azalan para miktarındna hesaplama yap diyoruz burada.
+      }    
     },
-
-    // handleChange: (state, action) => {
-    //   const { id, count} = action.payload;
-    //   //  her bir ürünün id'si ile item'ın id'si eşitse item'ı buluruz(her bir ürünümüz).
-    //   const item = state.items.find(x => x.id === id);
-    //   const oldCount = item.count;
-    //   const difference = count - oldCount;
-    //   const degisecekMiktar = Number(item.productPrice) * Math.abs(difference);
-    //   console.log('items', state.items);    
-    //   console.log('item', item);
-
-    //    if(difference >0){
-    //     state.value = Number(state.value) - Number(degisecekMiktar);
-    //    }else{
-    //     state.value = Number(state.value) + Number(degisecekMiktar);
-    //    }
-    // },
-    // updateCount: (state, action) => {
-    //   const { id, count} = action.payload;
-    //   //  her bir ürünün id'si ile item'ın id'si eşitse item'ı buluruz(her bir ürünümüz).
-    //   const item = state.items.find(x => x.id === id);
-    //   item.count = count;
-    // },
   },
-
   extraReducers: {
     [fetchProducts.fulfilled]: (state, action) => {
       state.items = action.payload;
-      // console.log("action.payload ", action.payload); //action.payload items'da gelen {} içindeki ürünlerimiz.
-      // const { id, count } = action.payload;
-      // const item = state.items.find((x) => x.id === id);
-      // item.count = count;
-      // console.log("items", state.items);
-      // console.log("item", item);
     },
-  },  
+  },
 });
 
-export const {updateCount, handleChange} = productsSlice.actions;
+export const { handleChange } = productsSlice.actions;
 export default productsSlice.reducer;
