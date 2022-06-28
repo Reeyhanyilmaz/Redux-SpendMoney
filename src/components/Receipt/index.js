3500d4947cd8f3f4c509ef2026659255f4169cd3
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-28 11:10:54 +0300

    add receipt total style

--- src/components/Receipt/index.js
+++ src/components/Receipt/index.js
@@ -1,34 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-function Receipt({newCount}) {
+function Receipt() {
   const receiptItems = useSelector((state) => state.products.receiptItems);
   const receiptMoney = useSelector((state) => state.products.receiptMoney);
   return (
-    <div>
-      <div className="receipt-div" style={{textAlign: "center", width: "300px"}}>
-        <h1>Your Receipt</h1>
-        {receiptItems.map((item, i) => (
-          <div
-            key={i}
-            className="receipt-items"
-            style={{
-              display: "flex",
-              flexDirection: "row"}}
-          >
-            <span style={{padding: "5px"}}>{item.productName}</span>        
-            <span style={{padding: "5px"}}>x {item.count}</span>
-            <span style={{ color: "green", padding: "5px" }}>${item.productPrice}</span>
+    <>
+      {receiptMoney > 0 ? (
+        <div className="receipt-div">
+          <h1>Your Receipt</h1>
+          {receiptItems.map((item, i) => (
+            <div key={i} className="receipt-items">
+              <span style={{ padding: "5px" }}>{item.productName}</span>
+              <span style={{ padding: "5px" }}>x {item.count}</span>
+              <span style={{ color: "green", padding: "5px" }}>
+                ${item.productPrice}
+              </span>
+            </div>
+          ))}
+          <hr style={{width: "300px"}}/>
+          <div className="receipt-total">
+            <span>Total: </span>
+            <span style={{ color: "green" }}>${receiptMoney}</span>
           </div>
-        ))}
-        <hr />
-        <div className="receipt-total">
-          <span>Total: </span>
-          <span style={{ color: "green" }}>${receiptMoney}</span>
-              
         </div>
-      </div>
-    </div>
+      ) : (
+        <h3>No items in your receipt</h3>
+      )}
+    </>
   );
 }
 
