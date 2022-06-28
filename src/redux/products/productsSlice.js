979bcdefa302cf7367407af2f04b67afb0fa906f
hakanolgun <kaptanhakanolgun@gmail.com>
2022-06-28 13:17:45 +0300

    fix receiptItems array

--- src/redux/products/productsSlice.js
+++ src/redux/products/productsSlice.js
@@ -32,36 +32,39 @@ export const productsSlice = createSlice({
       const item = state.items.find((item) => item.id === id);
       console.log("item", item);
 
+      state.oldMoney = state.newMoney;
+
       const countDifference = newCount - count; //count adedimiz , newCount basınca değişecek değerimiz.
 
-      const totalPrice = item.productPrice * Math.abs(countDifference);
+      const totalPrice = Number(item.productPrice) * Math.abs(countDifference);
 
       if (countDifference > 0) { //alım
-              if (totalPrice > state.newMoney) {
-                alert("You don't have enough money!");
-                return; //işlem yapma
-              } else {
-                state.newMoney = state.oldMoney - totalPrice;
-                item.count = newCount;
-                state.oldMoney = state.newMoney; //bunu yazmazsak diğer item aldığımızda tekrar 100 milyondan fiyat düşüyor.
-              }             
+        if (totalPrice > state.newMoney) {
+          alert("You don't have enough money!");
+          return; //işlem yapma
+        } else {
+          state.newMoney = state.oldMoney - totalPrice;
+          item.count = newCount;
+          // state.oldMoney = state.newMoney; //bunu yazmazsak diğer item aldığımızda tekrar 100 milyondan fiyat düşüyor.
+        }
       } else { //satış
         state.newMoney = state.oldMoney + totalPrice;
         item.count = newCount;
-        state.oldMoney = state.newMoney; //bunu yazmazsak diğer item satışında 100 milyona ekleme yapıyor. 
+        // state.oldMoney = state.newMoney; //bunu yazmazsak diğer item satışında 100 milyona ekleme yapıyor. 
         //Tekrar tekrar parayı 100 milyon yapma paramız eksilen/azalan para miktarındna hesaplama yap diyoruz burada.   
-      }   
-           
-      //for receipt part*******************************************        
-      const filtered = state.items.find(item => item.count > 0);
-      if(filtered){       
-        state.receiptItems.push(item);
-        // state.receiptItems.push(filtered);
-        item.count  = newCount;
-
-        //total değerini verecek kısım.
-        countDifference > 0 ? state.receiptMoney += totalPrice : state.receiptMoney -= totalPrice;      
       }
+
+      //for receipt part*******************************************        
+      const newReceiptItems = state.items.filter(item => item.count > 0); // receiptitems öğelerini bulduk
+      state.receiptItems = newReceiptItems;
+
+      let total = 0;
+      newReceiptItems.map((newReceiptItem) => {
+        let itemTotal = Number(newReceiptItem.count) * Number(newReceiptItem.productPrice)
+        total += itemTotal
+      })
+      state.receiptMoney = total;
+
     },
   },
   extraReducers: {
@@ -71,5 +74,5 @@ export const productsSlice = createSlice({
   },
 });
 
-export const { handleChange} = productsSlice.actions;
+export const { handleChange } = productsSlice.actions;
 export default productsSlice.reducer;
