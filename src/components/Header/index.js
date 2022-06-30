c067e0c16b01334927961f67bcee6188ee3f061e
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-30 17:33:36 +0300

    add user options

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import "./style.css"
+import "./style.css";
+import { useSelector, useDispatch } from "react-redux";
+import { changeSelectedUser } from "../../redux/products/productsSlice";
+import User from "../User";
+import { useState } from "react";
 
 function Header() {
+  const dispatch = useDispatch();
+  const whoseSpend = useSelector((state) => state.products.whoseSpend);
+  const [user, setUser] = useState(whoseSpend.name);
   return (
-    <div className='header-div'>
-        <img src = "https://neal.fun/spend/billgates.jpg" className='header-img' alt="headerImage"/>
-        Spend Bill Gates' Money
-    </div>
-  )
+    <>
+      <div className="selectMenu-div">
+        <p style={{ fontSize: "30px", fontWeigth: "400" }}>
+          Whose money do you want to spend?
+        </p>
+        <select
+          onChange={(e) => dispatch(changeSelectedUser(e.target.value))}
+          className="selectMenu"
+        >
+          {whoseSpend.map((user) => (
+            <option key={user.name} value={user.name}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <User />
+    </>
+  );
 }
 
 export default Header;
