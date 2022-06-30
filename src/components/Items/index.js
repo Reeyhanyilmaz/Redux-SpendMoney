f9ae1a4e1949166f92636234632c9fd662a49d7b
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-30 17:34:06 +0300

    add toast

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -2,6 +2,8 @@ import {useEffect} from 'react';
 import { useDispatch , useSelector} from 'react-redux';
 import { fetchProducts, handleChange } from '../../redux/products/productsSlice';
 import "./style.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Items(){
 
@@ -12,7 +14,24 @@ function Items(){
   },[dispatch]);
 
   const items = useSelector((state) => state.products.items);
-  
+  const noMoney = useSelector((state) => state.products.noMoney);
+
+  const notify = () => toast('😔 You have not enough money!', {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+
+  useEffect(() => {
+    if(noMoney === true){
+         notify();
+    }
+  },[noMoney])
+   
   return (
     <div className='items-div'>
       {
@@ -30,16 +49,16 @@ function Items(){
               Sell</button>
                <input type='number' min="0" className='count-span' value={product.count} 
                onChange={(e)=>dispatch(handleChange({id: product.id, count: Number(product.count), newCount: Number(e.target.value)}))} />
-              <button className='btn' 
-              // disabled={buyable} 
+              <button className='btn'
               onClick={(e) => dispatch(handleChange({id: product.id, count: Number(product.count), newCount: Number(product.count + 1)}))}
               style={{backgroundColor: "green", color: "white"}}>Buy</button>
+               <ToastContainer />
               </div>        
           </div>
         ))
       }
-      {/* <Receipt newCount={(e)=> Number(e.target.value)}/> */}
     </div>
+ 
   );
 }
 
